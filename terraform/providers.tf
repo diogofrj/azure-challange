de2dc41ef654c4020f07e51a5dd1b7a28d7a092c
Diogo Fernandes <dfs@outlook.com.br>
2024-11-03 19:53:59 -0300

    novo yml

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,16 +6,10 @@ terraform {
     }
   }
   required_version = ">= 1.0.0"
-  backend "azurerm" {
-    resource_group_name  = var.TF_VAR_ARM_BACKEND_RESOURCE_GROUP
-    storage_account_name = var.TF_VAR_ARM_BACKEND_STORAGE_ACCOUNT
-    container_name       = var.TF_VAR_ARM_BACKEND_CONTAINER
-    key                  = var.TF_VAR_ARM_BACKEND_KEY
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
   skip_provider_registration = true
 }
-
