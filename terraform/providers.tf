ef1a4bcf96d907e32ae745fd5cbeeca1e60990ef
Diogo Fernandes <dfs@outlook.com.br>
2024-11-03 19:13:23 -0300

    lets go :(

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.0.0"
+  backend "azurerm" {
+    resource_group_name  = var.TF_VAR_ARM_BACKEND_RESOURCE_GROUP
+    storage_account_name = var.TF_VAR_ARM_BACKEND_STORAGE_ACCOUNT
+    container_name       = var.TF_VAR_ARM_BACKEND_CONTAINER
+    key                  = var.TF_VAR_ARM_BACKEND_KEY
+  }
 }
 
 provider "azurerm" {
