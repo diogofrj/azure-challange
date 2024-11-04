dfacdff71d8c29a2cc5045c8c68e770cc98c53db
Diogo Fernandes <dfs@outlook.com.br>
2024-11-03 21:41:55 -0300

    agora vai

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,9 @@
 output "resource_group_name" {
-  value = module.rg.resource_group_name.name
+  value       = module.rg.resource_group_name
   description = "Resource Group Name"
+}
+
+output "resource_group_location" {
+  value       = module.rg.resource_group_location
+  description = "Resource Group Location"
 }
