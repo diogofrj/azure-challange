2cb7f014eeb632f5bcbfc7d06f85793f291c3766
Diogo Fernandes <dfs@outlook.com.br>
2024-11-03 21:23:54 -0300

    teste var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,42 +13,42 @@ variable "resource_group_name" {
   description = "Nome do Resource Group"
 }
 
-variable "address_space" {
-  type        = list(string)
-  description = "Address space da VNet"
-}
+# variable "address_space" {
+#   type        = list(string)
+#   description = "Address space da VNet"
+# }
 
-variable "shared_subnet_prefix" {
-  type        = string
-  description = "CIDR da subnet compartilhada"
-}
+# variable "shared_subnet_prefix" {
+#   type        = string
+#   description = "CIDR da subnet compartilhada"
+# }
 
-variable "inbound_subnet_prefix" {
-  type        = string
-  description = "CIDR da subnet de inbound"
-}
+# variable "inbound_subnet_prefix" {
+#   type        = string
+#   description = "CIDR da subnet de inbound"
+# }
 
-variable "mgmt_subnet_prefix" {
-  type        = string
-  description = "CIDR da subnet de gerenciamento"
-}
-variable "dns_zone_tags" {
-  type        = map(string)
-  description = "Tags para os recursos"
-  default     = {}
-}
+# variable "mgmt_subnet_prefix" {
+#   type        = string
+#   description = "CIDR da subnet de gerenciamento"
+# }
+# variable "dns_zone_tags" {
+#   type        = map(string)
+#   description = "Tags para os recursos"
+#   default     = {}
+# }
 
-variable "dns_zone_name" {
-  type        = string
-  description = "Nome da zona DNS privada"
-}
+# variable "dns_zone_name" {
+#   type        = string
+#   description = "Nome da zona DNS privada"
+# }
 
-variable "tags" {
-  type        = map(string)
-  description = "Tags para os recursos"
-  default     = {}
-}
-variable "route_table_name" {
-  type        = string
-  description = "Nome da route table"
-}
+# variable "tags" {
+#   type        = map(string)
+#   description = "Tags para os recursos"
+#   default     = {}
+# }
+# variable "route_table_name" {
+#   type        = string
+#   description = "Nome da route table"
+# }
